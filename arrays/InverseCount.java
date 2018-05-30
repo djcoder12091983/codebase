6c3564eea4c0eeff6204f703db926b6586d79e92
djcoder12091983 <debasis.jana.iit@gmail.com>
2018-05-30 17:10:23 +0530

    Add files via upload

---
+++ arrays/InverseCount.java
@@ -0,0 +1,81 @@
+package practice.computation.array;
+
+public class InverseCount {
+	
+	int data[];
+	
+	public InverseCount(int data[]) {
+		this.data = data;
+	}
+	
+	// start end inclusive
+	int count(int start, int end) {
+		if(start == end) {
+			//only one element
+			return 0;
+		}
+		int mid = start + (end-start)/2;
+		int l = count(start, mid);
+		int r = count(mid+1, end);
+		
+		// merge count
+		int merge = merge(start, mid, mid+1, end);
+		return l+r+merge;
+	}
+	
+	// start end inclusive
+	int merge(int start1, int end1, int start2, int end2) {
+		int newdata[] = new int[end2-start1+1];
+		
+		int i = start1;
+		int j = start2;
+		int k = 0;
+		int ic = 0;
+		while(i<=end1 && j<=end2) {
+			if(data[i]<=data[j]) {
+				newdata[k++] = data[i];
+				i++;
+			} else {
+				newdata[k++] = data[j];
+				// inverse count
+				ic += end1-i+1;
+				j++;
+			}
+		}
+		
+		// remaining first-set
+		while(i<=end1) {
+			newdata[k++] = data[i];
+			i++;
+		}
+
+		// remaining second-set
+		while(j<=end2) {
+			newdata[k++] = data[j];
+			j++;
+		}
+		
+		// copy to original array
+		k = start1;
+		for(int nd : newdata) {
+			data[k++] = nd;
+		}
+		
+		return ic;
+	}
+	
+	void print() {
+		for(int d : data) {
+			System.out.print(d + " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int data[] = new int[]{1, 1, 1, 2, 2};
+		InverseCount ic = new InverseCount(data);
+		System.out.println("Inverse Count: " + ic.count(0, data.length-1)); // count
+		System.out.println("Sorted-Data");
+		ic.print();
+	}
+
+}
